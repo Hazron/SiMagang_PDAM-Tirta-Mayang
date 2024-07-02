b5ddacf949f84b5eb3b4079d9e758112b4685645
Hazron <hazron2003redian@gmail.com>
2024-07-02 14:11:22 +0700

    feat: homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,75 @@
-<div>
-    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>SiMagang Tirta Mayang</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="{{ asset('assets_landing/img/favicon.png') }}" rel="icon">
+    <link href="{{ asset('assets_landing/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+
+
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('assets_landing/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets_landing/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets_landing/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets_landing/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets_landing/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="{{ asset('assets_landing/css/style.css') }}" rel="stylesheet">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+
+    <!-- =======================================================
+  * Template Name: Lumia
+  * Template URL: https://bootstrapmade.com/lumia-bootstrap-business-template/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+        <div class="container text-center text-md-left">
+            <h1><span>SiMagang Tirta Mayang</span></h1>
+            <h2>Aplikasi Presensi, Logbook untuk Peserta Magang di PDAM Tirta Mayang</h2>
+            <a href="{{ route('login') }}" class="btn-get-started scrollto">LOGIN</a>
+        </div>
+    </section><!-- End Hero -->
+
+
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="{{ asset('assets_landing/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+    <script src="{{ asset('assets_landing/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets_landing/vendor/glightbox/js/glightbox.min.js') }}"></script>
+    <script src="{{ asset('assets_landing/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets_landing/vendor/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('assets_landing/vendor/waypoints/noframework.waypoints.js') }}"></script>
+    <script src="{{ asset('assets_landing/vendor/php-email-form/validate.js') }}"></script>
+
+    <!-- Template Main JS File -->
+    <script src="{{ asset('assets_landing/js/main.js') }}"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+
+</body>
+
+</html>
